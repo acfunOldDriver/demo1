de9c7bb4caf4ab2e66b95dc11f81fdcd6ffe6fc7
刘冰傲 <baliu@ittx.com.cn>
2022-07-20 18:40:09 +0800

    1111

---
+++ component/ComponentA.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "ComponentA",
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+}
+</script>
+
+<style scoped>
+
+</style>
