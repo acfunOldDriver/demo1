6393757819eaa96c8be0087d92a3ec66beda1086
刘冰傲 <baliu@ittx.com.cn>
2022-07-22 18:37:17 +0800

    111

---
+++ src/components/login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="changeMsg">改变msg</button>
+    <el-row>
+      <el-button disabled>点我</el-button>
+      <el-button type="primary">按钮1</el-button>
+      <el-button type="success">按钮2</el-button>
+      <el-button type="info">按钮3</el-button>
+      <el-button type="warning">按钮4</el-button>
+    </el-row>
+    <el-row>
+      <el-button disabled>点我</el-button>
+      <el-button type="primary">按钮1</el-button>
+      <el-button type="success">按钮2</el-button>
+      <el-button type="info">按钮3</el-button>
+      <el-button type="warning">按钮4</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data(){
+    return {
+      msg:'aa'
+    }
+  },
+  methods: {
+    changeMsg:function () {
+      this.msg = Math.random()
+      axios.get()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history', //去掉链接的#号
+  base: '/', //去掉链接的/杠
   routes: [
-    {
+    { //重定向
       path: '/',
-      name: 'HelloWorld',
+      redirect: 'login',
+    },
+    { //重定向
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    { //重定向
+      path: '/HelloWorld',
+      name: 'hello',
       component: HelloWorld
     }
   ]
